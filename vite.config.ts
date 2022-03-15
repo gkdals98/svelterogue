b84112ad1f157ed6149263a30b944309757d937f
chm0326 <chm0326@chungdahm.com>
2022-03-15 17:48:55 +0900

    more directing

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,25 @@
 import { defineConfig } from 'vite'
+import EnvironmentPlugin from 'vite-plugin-environment'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { fileURLToPath, URL } from "url";
+
+// https://vitejs.dev/config/
+const commonSettings = {
+  plugins: [
+    EnvironmentPlugin(
+      {
+        //sample
+        'SAMPLE': 'sample!',
+      }
+    ),
+    svelte(),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    }
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
